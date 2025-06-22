7eb2cfb8ac0a0057c4862e4881cc7366d186aacf
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-06-22 23:09:59 +0800

    test: minify some tests

--- tests/test_builtin.lua
+++ tests/test_builtin.lua
@@ -1,92 +1,83 @@
 local builtin = require "obsidian.builtin"
 
-describe("util.wiki_link_id_prefix()", function()
-  it("should work without an anchor link", function()
-    MiniTest.expect.equality(
-      "[[123-foo|Foo]]",
-      builtin.wiki_link_id_prefix { path = "123-foo.md", id = "123-foo", label = "Foo" }
-    )
-  end)
+local new_set, eq = MiniTest.new_set, MiniTest.expect.equality
 
-  it("should work with an anchor link", function()
-    MiniTest.expect.equality(
-      "[[123-foo#heading|Foo ❯ Heading]]",
-      builtin.wiki_link_id_prefix {
-        path = "123-foo.md",
-        id = "123-foo",
-        label = "Foo",
-        anchor = { anchor = "#heading", header = "Heading", level = 1, line = 1 },
-      }
-    )
-  end)
-end)
+local T = new_set()
 
-describe("util.wiki_link_path_prefix()", function()
-  it("should work without an anchor link", function()
-    MiniTest.expect.equality(
-      "[[123-foo.md|Foo]]",
-      builtin.wiki_link_path_prefix { path = "123-foo.md", id = "123-foo", label = "Foo" }
-    )
-  end)
+T["wiki_link_id_prefix"] = new_set()
 
-  it("should work with an anchor link and header", function()
-    MiniTest.expect.equality(
-      "[[123-foo.md#heading|Foo ❯ Heading]]",
-      builtin.wiki_link_path_prefix {
-        path = "123-foo.md",
-        id = "123-foo",
-        label = "Foo",
-        anchor = { anchor = "#heading", header = "Heading", level = 1, line = 1 },
-      }
-    )
-  end)
-end)
+T["wiki_link_id_prefix"]["should work without an anchor link"] = function()
+  eq("[[123-foo|Foo]]", builtin.wiki_link_id_prefix { path = "123-foo.md", id = "123-foo", label = "Foo" })
+end
 
-describe("util.wiki_link_path_only()", function()
-  it("should work without an anchor link", function()
-    MiniTest.expect.equality(
-      "[[123-foo.md]]",
-      builtin.wiki_link_path_only { path = "123-foo.md", id = "123-foo", label = "Foo" }
-    )
-  end)
+T["wiki_link_id_prefix"]["should work with an anchor link"] = function()
+  eq(
+    "[[123-foo#heading|Foo ❯ Heading]]",
+    builtin.wiki_link_id_prefix {
+      path = "123-foo.md",
+      id = "123-foo",
+      label = "Foo",
+      anchor = { anchor = "#heading", header = "Heading", level = 1, line = 1 },
+    }
+  )
+end
 
-  it("should work with an anchor link", function()
-    MiniTest.expect.equality(
-      "[[123-foo.md#heading]]",
-      builtin.wiki_link_path_only {
-        path = "123-foo.md",
-        id = "123-foo",
-        label = "Foo",
-        anchor = { anchor = "#heading", header = "Heading", level = 1, line = 1 },
-      }
-    )
-  end)
-end)
+T["wiki_link_path_prefix"] = new_set()
 
-describe("util.markdown_link()", function()
-  it("should work without an anchor link", function()
-    MiniTest.expect.equality(
-      "[Foo](123-foo.md)",
-      builtin.markdown_link { path = "123-foo.md", id = "123-foo", label = "Foo" }
-    )
-  end)
+T["wiki_link_path_prefix"]["should work without an anchor link"] = function()
+  eq("[[123-foo.md|Foo]]", builtin.wiki_link_path_prefix { path = "123-foo.md", id = "123-foo", label = "Foo" })
+end
 
-  it("should work with an anchor link", function()
-    MiniTest.expect.equality(
-      "[Foo ❯ Heading](123-foo.md#heading)",
-      builtin.markdown_link {
-        path = "123-foo.md",
-        id = "123-foo",
-        label = "Foo",
-        anchor = { anchor = "#heading", header = "Heading", level = 1, line = 1 },
-      }
-    )
-  end)
+T["wiki_link_path_prefix"]["should work with an anchor link and header"] = function()
+  eq(
+    "[[123-foo.md#heading|Foo ❯ Heading]]",
+    builtin.wiki_link_path_prefix {
+      path = "123-foo.md",
+      id = "123-foo",
+      label = "Foo",
+      anchor = { anchor = "#heading", header = "Heading", level = 1, line = 1 },
+    }
+  )
+end
 
-  it("should URL-encode paths", function()
-    MiniTest.expect.equality(
-      "[Foo](notes/123%20foo.md)",
-      builtin.markdown_link { path = "notes/123 foo.md", id = "123-foo", label = "Foo" }
-    )
-  end)
-end)
+T["wiki_link_path_only"] = new_set()
+
+T["wiki_link_path_only"]["should work without an anchor link"] = function()
+  eq("[[123-foo.md]]", builtin.wiki_link_path_only { path = "123-foo.md", id = "123-foo", label = "Foo" })
+end
+
+T["wiki_link_path_only"]["should work with an anchor link"] = function()
+  eq(
+    "[[123-foo.md#heading]]",
+    builtin.wiki_link_path_only {
+      path = "123-foo.md",
+      id = "123-foo",
+      label = "Foo",
+      anchor = { anchor = "#heading", header = "Heading", level = 1, line = 1 },
+    }
+  )
+end
+
+T["markdown_link"] = new_set()
+
+T["markdown_link"]["should work without an anchor link"] = function()
+  eq("[Foo](123-foo.md)", builtin.markdown_link { path = "123-foo.md", id = "123-foo", label = "Foo" })
+end
+
+T["markdown_link"]["should work with an anchor link"] = function()
+  eq(
+    "[Foo ❯ Heading](123-foo.md#heading)",
+    builtin.markdown_link {
+      path = "123-foo.md",
+      id = "123-foo",
+      label = "Foo",
+      anchor = { anchor = "#heading", header = "Heading", level = 1, line = 1 },
+    }
+  )
+end
+
+T["markdown_link"]["should URL-encode paths"] = function()
+  eq("[Foo](notes/123%20foo.md)", builtin.markdown_link { path = "notes/123 foo.md", id = "123-foo", label = "Foo" })
+end
+
+return T
--- tests/test_util.lua
+++ tests/test_util.lua
@@ -77,6 +77,28 @@ T["is_whitespace"] = function()
   eq(false, M.is_whitespace "a  ")
 end
 
+T["is_hex_color"] = new_set()
+
+T["is_hex_color"]["recognizes valid hex colors"] = function()
+  eq(M.is_hex_color "#abc", true)
+  eq(M.is_hex_color "#abcd", true)
+  eq(M.is_hex_color "#aabbcc", true)
+  eq(M.is_hex_color "#aabbccdd", true)
+end
+
+T["is_hex_color"]["rejects invalid hex colors"] = function()
+  eq(M.is_hex_color "#ab", false)
+  eq(M.is_hex_color "#abcde", false)
+  eq(M.is_hex_color "#aabbccfg", false)
+  eq(M.is_hex_color "#aabbccdde", false)
+end
+
+T["is_hex_color"]["rejects invalid chars"] = function()
+  eq(M.is_hex_color "#ggg", false)
+  eq(M.is_hex_color "#12345z", false)
+  eq(M.is_hex_color "#xyzxyz", false)
+end
+
 T["count_indent"] = new_set()
 
 T["count_indent"]["should count each space as one indent"] = function()
