3fde864253d5a2ead0c822205e350f2169c113bd
Zizhou Teng <412445606@qq.com>
2025-06-26 18:22:41 +0800

    fix: add lsp mod

---
+++ lua/obsidian/lsp/init.lua
@@ -0,0 +1,35 @@
+local lsp = {}
+
+--- Start the lsp client
+---
+---@param client obsidian.Client
+---@param buf integer
+---@return integer
+lsp.start = function(client, buf)
+  local handlers = require "obsidian.lsp.handlers"
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+  local client_id = vim.lsp.start({
+    name = "obsidian-ls",
+    capabilities = capabilities,
+    cmd = function()
+      return {
+        request = function(method, params, handler, _)
+          handlers[method](client, params, handler, _)
+        end,
+        notify = function(method, params, handler, _)
+          handlers[method](client, params, handler, _)
+        end,
+        is_closing = function() end,
+        terminate = function() end,
+      }
+    end,
+    init_options = {},
+    root_dir = tostring(client.dir),
+  }, { bufnr = buf })
+  assert(client_id, "failed to start obsidian_ls")
+
+  return client_id
+end
+
+return lsp
