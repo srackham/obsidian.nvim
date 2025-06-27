41e994b083f353407a9c0693fdcae033ea92c359
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-06-27 14:09:09 +0800

    fix: correct command args

--- lua/obsidian/commands/rename.lua
+++ lua/obsidian/commands/rename.lua
@@ -1,4 +1,9 @@
 ---@param data CommandArgs
 return function(_, data)
-  vim.lsp.buf.rename(vim.trim(data.args))
+  local new_name = vim.trim(data.args)
+  if #new_name == 0 then
+    vim.lsp.buf.rename()
+  else
+    vim.lsp.buf.rename(new_name)
+  end
 end
