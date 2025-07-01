87ad1d274f40341daeb4639a445abdf928f1dc8e
zizhou teng (n451) <2020200706@ruc.edu.cn>
2025-07-01 20:40:25 +0800

    fix: remove unused helper

--- lua/obsidian/api.lua
+++ lua/obsidian/api.lua
@@ -308,26 +308,6 @@ M.open_buffer = function(path, opts)
   return result_bufnr
 end
 
----Get an iterator of (bufnr, bufname) over all named buffers. The buffer names will be absolute paths.
----
----@return function () -> (integer, string)|?
-M.get_named_buffers = function()
-  local idx = 0
-  local buffers = vim.api.nvim_list_bufs()
-
-  ---@return integer|?
-  ---@return string|?
-  return function()
-    while idx < #buffers do
-      idx = idx + 1
-      local bufnr = buffers[idx]
-      if vim.api.nvim_buf_is_loaded(bufnr) then
-        return bufnr, vim.api.nvim_buf_get_name(bufnr)
-      end
-    end
-  end
-end
-
 ----------------
 --- text api ---
 ----------------
